ee32495bbe286f35fb6b1cee4071024750d68997
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-01-04 18:08:12 -0500

    Create function for each game round, create helper function to determine winner

--- script.js
+++ script.js
@@ -22,9 +22,54 @@ function getHumanChoice(){
   let userAnswer = prompt('Please Make A Choice: Rock, Paper or Scissors?');
 
   // If the user's input falls into one of three choices, return the choice
-  if(userAnswer === 'Rock' || userAnswer === 'Paper' || userAnswer === 'Scissors'){
+  if(userAnswer.toLowerCase() === 'rock' || userAnswer.toLowerCase() === 'paper' || userAnswer.toLowerCase === 'scissors'){
     return userAnswer;
   }
 
   return console.error('It looks like you gave an invalid answer');
+}
+
+function determineWinner(humanChoice, computerChoice){
+  // Rock Beats Scissors
+  if(humanChoice === 'rock' && computerChoice === 'scissors'){
+    humanScore += 1;
+    return `Hooray, You win! Rock beats Scissors. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  } else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+    computerScore += 1;
+    return `Aw, You lose; Scissors loses aganist Rock. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  }
+
+  // Scissors Beats Paper
+  if(humanChoice === 'scissors' && computerChoice === 'paper'){
+    humanScore += 1;
+    return `Hooray, You win! Scissors beats Paper. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  } else if(humanChoice === 'paper' && computerChoice === 'scissors'){
+    computerScore += 1;
+    return `Aw, You lose; Paper loses aganist Scissors. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  }
+
+  // Paper Beats Rock
+  if(humanChoice === 'paper' && computerChoice === 'rock'){
+    humanScore += 1;
+    return `Hooray, You win! Paper beats Rock. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  } else if(humanChoice === 'rock' && computerChoice === 'paper'){
+    computerScore += 1;
+    return `Aw, You lose; Rock loses aganist Paper. You have ${humanScore} points and the computer has ${computerScore} points.`;
+  }
+
+  // If it's a tie
+  if(humanChoice === computerChoice){
+    return 'Looks like it\'s a draw! No points!'
+  }
+}
+
+function playRound(humanChoice, computerChoice){
+  // Make the choices lowercase for case insensitive comparisons
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  // Determine the winner and store into the result variable
+  let result = determineWinner(humanChoice, computerChoice);
+  // Console log the result
+  console.log(result);
 }
